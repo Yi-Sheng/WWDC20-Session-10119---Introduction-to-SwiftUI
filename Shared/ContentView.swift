e4ac7273543f681d3c18518098813b8ea9dcfa66
Eason <icecomic5566@livemail.tw>
2020-06-27 11:33:09 +0800

    Initial Commit

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -8,13 +8,37 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var sandwiches = [Sandwich]()
+    
     var body: some View {
-        Text("Hello, world!").padding()
+        
+        NavigationView {
+            
+            List(sandwiches) { sandwich in
+                
+                NavigationLink(
+                    destination: Text(sandwich.name)) {
+                    
+                    Image(sandwich.thumbnailName)
+                        .frame(width: 50, height: 50)
+                        .cornerRadius(8)
+                        
+                    VStack(alignment: .leading) {
+                        Text(sandwich.name)
+                        Text("\(sandwich.ingredientCount) ingredients")
+                            .font(.subheadline)
+                            .foregroundColor(.secondary)
+                    }
+                }
+            }
+            .navigationTitle("Sandwiches")
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(sandwiches: testData)
     }
 }

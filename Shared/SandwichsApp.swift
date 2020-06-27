172265ea9dd3453d1ecf3aa854864483f0fe37f6
Eason <icecomic5566@livemail.tw>
2020-06-27 10:58:35 +0800

    Initial Commit

---
+++ Shared/SandwichsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SandwichsApp.swift
+//  Shared
+//
+//  Created by Yi-Sheng Hsu on 2020/6/27.
+//
+
+import SwiftUI
+
+@main
+struct SandwichsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
